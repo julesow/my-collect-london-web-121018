843c67f6e4da72f2e2ed04b923b9eda9335e9aa1
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-30 15:12:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  new_array =[]
+  i = 0
+  while i < array.length 
+    yield array[i]
+    i += 1
+  end 
+  return new_array
+end
 
-
+# collection = ['ruby', 'javascript', 'python', 'objective-c']
+# my_collect(collection) do |lang|
+#   lang.upcase
+# end
